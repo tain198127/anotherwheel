eb70395eee2e07b75969500df8306254629bc833
DaneBrown <tain198127@163.com>
2017-07-05 13:38:39 +0800

    调整目录

--- framework/src/test/java/MainTest.java
+++ framework/src/test/java/MainTest.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.danebrown;
 
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
